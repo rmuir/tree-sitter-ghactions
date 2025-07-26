1b69837a4b0505111e2a8acc85c521ea67226e90
Robert Muir <rmuir@apache.org>
2025-07-26 14:30:31 -0400

    ci: initial setup

--- bindings/swift/TreeSitterActionsTests/TreeSitterActionsTests.swift
+++ bindings/swift/TreeSitterActionsTests/TreeSitterActionsTests.swift
@@ -1,12 +1,18 @@
-import XCTest
 import SwiftTreeSitter
 import TreeSitterActions
+import XCTest
 
 final class TreeSitterActionsTests: XCTestCase {
     func testCanLoadGrammar() throws {
         let parser = Parser()
         let language = Language(language: tree_sitter_actions())
-        XCTAssertNoThrow(try parser.setLanguage(language),
-                         "Error loading Github Actions grammar")
+        XCTAssertNoThrow(
+            try parser.setLanguage(language),
+            "Error loading Github Actions grammar")
+        if let tree = parser.parse("${{ example }}"), let root = tree.rootNode {
+            XCTAssertFalse(root.hasError, root.sExpressionString!)
+        } else {
+            XCTFail("Error parsing Github Actions document")
+        }
     }
 }
