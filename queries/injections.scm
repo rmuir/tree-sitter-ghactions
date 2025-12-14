80cb6e5c2b7bf4f5f741ce11aad921dcc0e8b932
Robert Muir <rmuir@apache.org>
2025-12-14 09:55:07 -0500

    feat: highlight string constant as first argument to format()
    This isn't technically a nim format string, but it is close enough and
    does the job well. It makes format() uses much easier to read.

--- queries/injections.scm
+++ queries/injections.scm
@@ -7,3 +7,13 @@
       (string_fragment) @injection.content) .)
   (#any-of? @_name "fromJSON" "fromJson")
   (#set! injection.language "json"))
+
+; string constant first arg to format()
+(function_call
+  name: (identifier) @_name
+  arguments: (argument_list
+    (string
+      .
+      (string_fragment) @injection.content))
+  (#eq? @_name "format")
+  (#set! injection.language "nim_format_string"))
